4b2d1bdab2d42f9766749c9656de706a33480a4b
Daria <dwein05@gmail.com>
2019-03-02 00:18:04 +0300

    changes

--- src/js/ES6/main.js
+++ src/js/ES6/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () { //call when DOM is ready
     let navigation_menu = document.querySelector('.menu'); //find menu container
 
-    document.querySelector('#menu__icon').addEventListener('click', function () { // find trigger button / add event listener
+    document.querySelector('#menu_icon').addEventListener('click', function () { // find trigger button / add event listener
         navigation_menu.classList.toggle('show'); //add/remove class
         document.body.classList.toggle('mobile_menu_open'); // fix body when menu is open
     })
